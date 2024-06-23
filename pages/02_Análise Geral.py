4d32cafcf719863d91e2743bee11defae041a4ed
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2024-06-23 19:48:27 -0300

    setores

--- pages/02_Análise Geral.py
+++ pages/02_Análise Geral.py
@@ -10,7 +10,7 @@ st.title("Analisando os Dados RAIS - São Paulo")
 with open("./src/css/style.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
     
-tab1, tab2 = st.tabs(["Empregos formais", "Salário"])
+tab1, tab2,tab3 = st.tabs(["Empregos formais", "Salário","Setores Produtivos"])
 
 
 with tab1:
@@ -54,7 +54,7 @@ with tab1:
     )
     fig_ano.update_layout(
         title=dict(
-            text='Quantidade de Registros por Ano',
+            text='Quantidade de Empregos Formais por Ano',
             font=dict(
                 family='Arial, sans-serif',
                 size=24,
@@ -306,4 +306,133 @@ with tab2:
 
     st.plotly_chart(fig_menor)
 
-      
+    st.markdown("""
+    ## Resultados da Análise
+    ### Média Salarial
+    O gráfico abaixo mostra a evolução da média salarial no estado de São Paulo ao longo dos anos.
+
+
+    ### Menor Salário
+    O gráfico abaixo mostra a variação do menor salário ao longo dos anos, considerando apenas empresas com um único funcionário.
+
+
+    ### Maior Salário
+    O gráfico abaixo ilustra a evolução do maior salário no estado de São Paulo, refletindo possíveis picos em setores específicos.
+
+
+    ### Observações
+    **Média Salarial**: A média salarial apresenta uma tendência de crescimento ao longo dos anos, indicando um aumento geral nos rendimentos dos empregados formais no estado de São Paulo.
+
+    **Menor Salário**: O menor salário, ao considerarmos apenas empresas com um único funcionário, mostrou variações menores ao longo dos anos, mas ainda assim um aumento gradual, refletindo possíveis ajustes salariais mínimos.
+
+    **Maior Salário**: O maior salário mostra variações mais significativas, possivelmente refletindo rendimentos excepcionais em alguns anos específicos.
+
+    ###Conclusão
+    A análise descritiva dos salários no estado de São Paulo revela uma tendência de aumento nos rendimentos médios dos empregados formais ao longo dos anos. Enquanto o menor salário aumentou de forma mais estável, o maior salário variou mais significativamente, refletindo aumentos em setores específicos ou posições de alto rendimento.
+    """)
+
+with tab3:
+    SetorgeralRaiS = pd.read_csv('./src/data/tratado/Raisempregoporsetor.csv', sep=';', index_col=False)
+
+    st.markdown("""
+        ## Análise Geral dos Empregos Formais por Setor
+
+        ### Introdução ao CNAE
+
+        A **Classificação Nacional de Atividades Econômicas (CNAE)** é um sistema utilizado pelo governo brasileiro para categorizar e organizar as atividades econômicas no país. Essa classificação é essencial para a análise econômica, pois permite agrupar e comparar dados de diferentes setores de forma padronizada.
+
+        ### Os Três Grandes Grupos de Setores
+
+        A CNAE divide todas as atividades econômicas em três grandes grupos principais:
+
+        - **Agricultura**: Inclui atividades relacionadas ao cultivo de plantas, criação de animais, produção florestal e pesca.
+        - **Indústria**: Abrange as atividades de transformação de bens e produtos, incluindo a manufatura, construção e outras atividades industriais.
+        - **Serviços**: Envolve uma ampla gama de atividades que não produzem bens tangíveis, como comércio, transporte, educação, saúde e serviços financeiros.
+
+        ## Primeira Análise
+
+        Nesta primeira etapa da nossa análise, vamos focar na distribuição de empregos formais entre esses três grandes grupos. Entender como os empregos são distribuídos entre Agricultura, Indústria e Serviços nos dará uma visão geral da estrutura econômica do estado de São Paulo.
+
+    """)
+
+    fig_setor = go.Figure()
+
+
+    setores = SetorgeralRaiS['SETOR'].unique()
+    for setor in setores:
+        setor_data = SetorgeralRaiS[SetorgeralRaiS['SETOR'] == setor]
+        fig_setor.add_trace(
+            go.Scatter(
+                x=setor_data['Ano'],
+                y=setor_data['Emprego_formal'],
+                mode='lines+markers',
+                name=setor,
+                text=setor_data['SETOR'],
+                hovertemplate='<b>%{x}</b><br>Emprego Formal: %{y}<br>Setor: %{text}<extra></extra>'
+            )
+        )
+
+    # Configurar layout do gráfico
+    fig_setor.update_layout(
+        title=dict(
+            text='Emprego Formal por Setor ao Longo dos Anos',
+            font=dict(family='Arial, sans-serif', size=24, color='#CD8D00'),
+            x=0.5,  # Centralizado horizontalmente
+            y=0.9,  # Perto do topo
+            xanchor='center',
+            yanchor='top'
+        ),
+        xaxis=dict(
+            title='Ano',
+            titlefont=dict(size=16, color='#004175'),
+            gridcolor='lightgray',        
+            showline=True,        
+            mirror=True,
+            tickangle=45,
+            tickformat='%Y',  
+        ),
+        yaxis=dict(
+            title='Emprego Formal',
+            titlefont=dict(size=16, color='#004175'),
+            gridcolor='lightgray',        
+        ),
+    )
+
+    # Exibir o gráfico no Streamlit
+    st.plotly_chart(fig_setor)
+                
+    st.markdown("""
+                
+    ## Análise Descritiva dos Empregos Formais por Setor no Estado de São Paulo
+
+    Conforme podemos observar no gráfico acima, o estado de São Paulo apresenta uma distribuição significativa de empregos formais no setor de **Serviços**, seguido pela **Indústria** e **Agropecuária**. Essa distribuição reflete a estrutura econômica do estado, que é o maior centro econômico do Brasil.
+
+    ### Setor de Serviços
+
+    O setor de serviços é o maior empregador formal em São Paulo, com uma média de mais de 10 milhões de empregos formais ao longo dos anos. Esse setor inclui uma vasta gama de atividades, como comércio, transporte, educação, saúde e serviços financeiros. A predominância do setor de serviços pode ser atribuída ao fato de São Paulo ser a capital financeira do país, abrigando as sedes das principais empresas e instituições financeiras, bem como uma infraestrutura robusta de comércio e serviços.
+
+    ### Setor Industrial
+
+    A indústria, embora menor que o setor de serviços, ainda desempenha um papel crucial na economia paulista, com aproximadamente 3 milhões de empregos formais em média. A presença de grandes polos industriais e parques tecnológicos em cidades como São Bernardo do Campo, Campinas e Sorocaba contribui significativamente para esses números. A indústria paulista é diversificada, abrangendo manufatura, construção civil, automotiva e muitas outras subáreas.
+
+    ### Setor Agropecuário
+
+    O setor agropecuário, apesar de ser o menor dos três em termos de emprego formal, continua sendo essencial para a economia do estado, especialmente nas regiões interioranas. Com uma média de cerca de 300 mil empregos formais, esse setor inclui atividades agrícolas, pecuárias e florestais. A produção agropecuária de São Paulo é diversificada e inclui cana-de-açúcar, laranja, café e carne bovina, produtos que são importantes tanto para o mercado interno quanto para a exportação.
+
+    ### Tendências ao Longo dos Anos
+
+    - **Serviços**: O setor de serviços mostrou um crescimento constante, com um leve declínio em 2016, possivelmente devido à crise econômica, seguido por uma recuperação sólida até 2021.
+    - **Indústria**: A indústria sofreu uma queda significativa entre 2014 e 2017, refletindo as dificuldades econômicas enfrentadas pelo país, mas começou a mostrar sinais de recuperação nos anos subsequentes.
+    - **Agropecuária**: A agropecuária manteve-se relativamente estável ao longo dos anos, com pequenas flutuações que refletem as safras anuais e mudanças climáticas.
+
+    ### Conclusão
+
+    A análise dos empregos formais nos setores de Agropecuária, Indústria e Serviços no estado de São Paulo revela uma economia diversificada e resiliente. O setor de serviços destaca-se como o pilar do mercado de trabalho formal, refletindo a importância da capital paulista como centro econômico e financeiro do Brasil. A indústria, apesar dos desafios, continua sendo uma força significativa, enquanto a agropecuária mantém sua relevância, especialmente nas regiões interioranas.
+
+    Essa análise fornece uma visão geral crucial para entender a estrutura econômica de São Paulo. No próximo passo, exploraremos em mais detalhes os fatores que impulsionam essas tendências e como podemos usar essas informações para fomentar um desenvolvimento econômico sustentável e inclusivo.
+
+    ### Dados e Metodologia
+
+    Utilizamos dados fornecidos pelo governo, especificamente a tabela do CNAE, para categorizar os empregos formais de acordo com esses três grandes grupos. A partir dessa categorização, podemos observar como cada grupo contribui para o mercado de trabalho formal.
+
+    """)
--- tratamento.py
+++ tratamento.py
@@ -1,37 +1,16 @@
 import pandas as pd 
 
 RaisBD = pd.read_csv('./src/data/raisbd.csv',sep=';',encoding='latin1',index_col=False)
+CneaBD = pd.read_excel('./src/data/cnae_20.xlsx',dtype={'Cod_Classe': str})
+
+RaisBD['CLAS_CNAE_20'] = RaisBD['CLAS_CNAE_20'].astype(str)
+CneaBD['Cod_Classe'] = CneaBD['Cod_Classe'].astype(str)
+
+result= pd.merge(RaisBD, CneaBD, left_on='CLAS_CNAE_20', right_on='Cod_Classe')
+
+result = result.groupby(['Ano', 'SETOR'])['Emprego_formal'].sum().reset_index()
+
+result = result.sort_values(by=['Ano', 'SETOR'])
 
 
-RaisBD['salario_individual'] = RaisBD['massa_rendimentos'] / RaisBD['Emprego_formal']
-
-RaisBD = RaisBD[RaisBD['salario_individual'] > 0]
-RaisBD = RaisBD[RaisBD['EMP_C_SAL'] > 0]
-RaisBD = RaisBD[RaisBD['massa_rendimentos'] > 0]
-
-# Agrupar por ano e calcular as somas necessárias
-agrupado = RaisBD.groupby('Ano').agg({
-    'Emprego_formal': 'sum',
-    'massa_rendimentos': 'sum'
-}).reset_index()
-
-# Calcular a média salarial por ano
-agrupado['media_salarial'] = agrupado['massa_rendimentos'] / agrupado['Emprego_formal']
-
-# Calcular o menor e maior salário individual por ano
-menor_salario = RaisBD[RaisBD['Emprego_formal'] == 1].groupby('Ano')['salario_individual'].min().reset_index()
-maior_salario = RaisBD.groupby('Ano')['salario_individual'].max().reset_index()
-
-# Combinar os resultados em uma única tabela
-analise_salarial = agrupado.merge(menor_salario, on='Ano').merge(maior_salario, on='Ano')
-analise_salarial.columns = ['Ano', 'Emprego Formal', 'Massa de Rendimentos', 'Média Salarial', 'Menor Salário', 'Maior Salário']
-
-# Formatar os valores para exibição
-analise_salarial['Massa de Rendimentos'] = analise_salarial['Massa de Rendimentos'].apply(lambda x: f'{x:,.2f}')
-analise_salarial['Média Salarial'] = analise_salarial['Média Salarial'].apply(lambda x: f'{x:,.2f}')
-analise_salarial['Menor Salário'] = analise_salarial['Menor Salário'].apply(lambda x: f'{x:,.2f}')
-analise_salarial['Maior Salário'] = analise_salarial['Maior Salário'].apply(lambda x: f'{x:,.2f}')
-
-print(analise_salarial)
-
-analise_salarial.to_csv('./src/data/tratado/raisbdanalisesalarial.csv',sep=';',index=False)
+result.to_csv('./src/data/tratado/Raisempregoporsetor.csv', sep=';', index=False)
